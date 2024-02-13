f6e49222769f08a99469c0bd6f9363b18efdc89f
wts98 <wtszshing98@gmail.com>
2024-02-13 16:59:31 +0800

    New Update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import re #regex
 import sys
 import magic #magic attr
 import glob #file only unix matching
+import logging
 def copytree(src, dst, symlinks=False, ignore=None): #monkeypatching shutil.copytree, will return an error upon use
     for item in os.listdir(src):
         s = os.path.join(src, item)
@@ -17,6 +18,16 @@ def copytree(src, dst, symlinks=False, ignore=None): #monkeypatching shutil.copy
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
+
+def print_matched_lines(regex, text): #custom regex function to print matched line by line
+    lines = text.split('\n')
+    for i, line in enumerate(lines, start=1):
+        matches = re.findall(regex, line)
+        if matches:
+            print(f"Line {i}: {line}")
+            for match in matches:
+                print(f" - Match: {match}")
+
 
 verbose=0
 # Parser init
@@ -68,29 +79,36 @@ if 1 <= args.Run_OP <= 3: #Check if run option is out of 1-3 range
                         else:
                             print(le.strip()," is not valid path")
         # Get bash history
-        
+        with open('/proc/sys/kernel/pidmax', 'r') as MaxPIDfile:
+            MaxPID = MaxPIDfile.read().rstrip() #Get Max PID value of the host system
         if init == 'systemd': #check if init system is systemd
             srvs_enabled = open("srvs.lst.enabled", "w")
-            subprocess.call(['systemctl', 'list-units', '--type=service', '--state=enabled'], stdout=srvs_enabled) #Get services in systemd style
+            subprocess.call(['systemctl', 'list-unit-files', '--type=service', '--state=enabled'], stdout=srvs_enabled) #Get services in systemd style
+            subprocess.call(['sed','-i' , '1d;$d', 'srvs.list.enabled',])
+            srvs_enabled.close()
             srvs_disabled = open("srvs.lst.disabled", "w")
-            subprocess.call(['systemctl', 'list-units', '--type=service', '--state=disabled'], stdout=srvs_disabled) #Get services in systemd style
+            subprocess.call(['systemctl', 'list-unit-files', '--type=service', '--state=disabled'], stdout=srvs_disabled) #Get services in systemd style
+            subprocess.call(['sed','-i' , '1d;$d', 'srvs.list.disabled',])
+            srvs_disabled.close()
             srvs_static = open("srvs.lst.static", "w")
-            subprocess.call(['systemctl', 'list-units', '--type=service', '--state=static'], stdout=srvs_static) #Get services in systemd style
-            srvs_masked = open("srvs.lst", "w")
-            subprocess.call(['systemctl', 'list-units', '--type=service', '--state=masked'], stdout=masked) #Get services in systemd style
-            syslog = open("syslog.log", "w")
-            subprocess.call(['journalctl', '--no-pager'], stdout=syslog) #Get all journal logs in systemd style
+            subprocess.call(['systemctl', 'list-unit-files', '--type=service', '--state=static'], stdout=srvs_static) #Get services in systemd style
+            subprocess.call(['sed','-i' , '1d;$d', 'srvs.list.static',])
+            srvs_static.close()
+            syslog = open("syslog", "w")
+            subprocess.call(['journalctl', '--no-pager'], stdout=syslog) #Get all journal logs in systemd style 
+            syslog.close()
         elif init == 'sysvinit':
             srvs = open("srvs.lst", "w")
             subprocess.call([ 'service', '--status-all'], stdout=srvs) #Get all service in Sysvinit style
             shutil.copy2('/etc/auth.log', destination)
             #shutil.copy2 ##syslog copy?
+            shutil.copy2('/etc/var/log/syslog', destination)
         else:
             print(init, " is not supported by this program for now!!")
 
         shutil.copy2('/etc/os-release', destination)
 
-        # Obtain command history PEr user.
+        # Obtain command history Per user.
         maindir = Path('/userlist')
         maindir.mkdir(exist_ok=True)
         histptn = r'^\.[A-Za-z0-9_-]+_history$' #Match all history file format
@@ -114,22 +132,36 @@ if 1 <= args.Run_OP <= 3: #Check if run option is out of 1-3 range
         #check if var file is in current directory
         if (Path.cwd()/'var').exists() == True:
             #if "syslog.log" and "srvs.st" : (both should exist)
-            if (Path.cwd()/"SourcePath.txt").exists() == True and (Path.cwd()/"syslog.log").exists() == True and (Path.cwd()/"srvs.lst").exists() == True :
+            if (Path.cwd()/"SourcePath.txt").exists() == True and (Path.cwd()/"syslog").exists() == True and (Path.cwd()/"srvs.lst").exists() == True :
                 #var is exist
                 with open("CaughtLogs.txt", "a") as c:
                     files = [ f for f in os.listdir('./var/') if os.path.is.dir(f)] #get file name
                     matchtype = r"^.log$"
-                    os.makedirs('./service-list')
-                    os.makedirs('./syslog/')
+                    #os.makedirs('./service-list')
+                    #os.makedirs('./syslog/')
                     #check init: if sysvinit
                     if init == 'systemd':
+                        if (Path.cwd()/"syslog.log").exist == True:
+                            logging.debug('Systemd JournalLog is found')
+                        else:
+                            logging.debug('Systemd JournalLog is not found')
+                            if (Path.cwd()/"").exist is == True:
+                                logging.debug('')
                         #two different mapping
                         #systemd style
-                    elif init == 'sysvinit'
+                    elif init == 'sysvinit':
+                        if (Path.cwd()/"syslog").exists == True:
+                            logging.debug('syslog is found')
+                        else:
+                            logging.debug('syslog is not fonud')
+                            if (Path.cwd()/"auth.log"):
+                                logging.debug('auth.log is found')
+                            else:
+                                logging.debug('auth.log is not found')
                         #sysvinit
                                 
 
-                    for i in range(len(files)): #Check Presence file?
+                    for i in range(len(files)): #Check Presence file? in /var/log?
                         if re.match(r'.*\.log$', files[i]):
                             #match auth?
                             c.writelines(str(i)+"\n")
@@ -145,6 +177,40 @@ if 1 <= args.Run_OP <= 3: #Check if run option is out of 1-3 range
     # func(Log Classification)
     elif args.Run_OP == 3:
         print("Running Log Summarization")
+        if (Path.cwd()/'CaughtLogs.txt').exist() == True:
+            #statement
+            file = open('CaughtLogs.txt', 'r') #Start of Custom Path
+            filelines = file.readlines()
+            for le in file:
+                if not os.path.exists(le):
+                    print(le, " is not found within the previous log collection")
+                else:
+                    # R E G E X T I M E // re.match from file
+                    #VV the following function should able to be reused per each function? But will need to implement how to organize the output as well.
+                    regex=r"ROOT"gm #regex string e.g. root, whatever protocol it might be #root global matching
+                    with open(le, 'r') as file:
+                        text=file.read()
+                    print_matched_lines(regex, text)
+                    ##below are for auth.log
+                    regex=r"\w\w\w\s\d+\s\d{2}:\d{2}:\d{2}\s"gm #for date    
+                    #{Mon} {D} {HH:MM:SS} <hostname> <cmd/util like sudo,login etc>: pam_unix(cmd:session): session opened for user root by <username> (uid=0)
+                    #{Mon} {D} {HH:MM:SS} <hostname> sudo: pam_unix(sudo:session): session opened for user root by <username> (uid=0)
+                    #{Mon} {D} {HH:MM:SS} <hostname> login[pid]: pam_unix(login:session): session opened for user root by <username> (uid=0)
+                    #{Mon} {D} {HH:MM:SS} <hostname> login[pid]: pam_unix(login:auth): authentication failure, logname=<service name like postgres uid=<#> euid=<#> tty=tty1 ruser= rhost= user=<username>
+
+                    ##below are for syslog and daemon.log (too low level?)
+                    #{Mon} {D} {HH:MM:SS} <hostname> /usr/sbin/cron[pid]: (CRON) {INFO/STARTUP}
+                    #{Mon} {D} {HH:MM:SS} <hostname> <service name>: 
+                    #{Mon} {D} {HH:MM:SS} <hostname> init: tty<#> main process (#pid) killed by <type of> signal
+                    ##below are for vsftpd.log
+                    #{WEK} {MON} {D} {HH:MM:SS} {YYYY} [pid ####] CONNECT: Client "<ipv4 ip>"
+                    #{WEK} {MON} {D} {HH:MM:SS} {YYYY} [pid ####] [<username>] OK LOGIN: Client "<ipv4 ip>"
+
+
+        else:
+            print('Missing Log verification and gathering!')
+
+
     # func(Log Summarization)
     else:
         print(le ," is Invalid Option!!!")
